f76b4a906b508a6bb58f23f0d75080414732ddd9
Kijifa <jiri.kifa@gmail.com>
2023-02-04 18:27:37 +0100

    Convert perfect canteen pdf to txt

--- restaurants/perfectCanteen.py
+++ restaurants/perfectCanteen.py
@@ -1,11 +1,17 @@
 from scrape.scrape import wget_download_page
-
+from scrape.scrape import convert_pdf
+from scrape.scrape import save_txt
 
 def perfect_canteen():
     url = "https://menu.perfectcanteen.cz/pdf/28/cz/price/a3"
     file_name = "perfect_canteen.pdf"
 
-    file = wget_download_page(url, file_name)
-    print(file)
+    canteen_file = wget_download_page(url, file_name)
+    print(canteen_file)
+    text = convert_pdf(canteen_file)
+    print(text)
+    save_txt("perfect_canteen.txt", text)
+
+
 
 
